18eb6ef638b2b61061aba0751f298c22b0ed1bc4
Sriram <110121885+SriramX6@users.noreply.github.com>
2024-07-15 22:10:19 +0530

    initial commit

--- index.js
+++ index.js
@@ -53,9 +53,12 @@ app.use(cors())
 // app.use(express.json({limit: '25mb'}));
 // app.use(express.urlencoded({limit: '25mb'}));
 
+// "mongodb://the-database:27017/bike"
+
+const uri = 'mongodb+srv://host:strongpassword@bharatbike.yb9eewp.mongodb.net/?retryWrites=true&w=majority&appName=bharatbike'
 const ac = () => { 
-    try{mongoose.connect("mongodb://the-database:27017/bike")
-    //.log("connected to mongodb")        
+    try{mongoose.connect(uri)
+    console.log("connected to mongodb")        
 }
     catch(error){
         throw error;
